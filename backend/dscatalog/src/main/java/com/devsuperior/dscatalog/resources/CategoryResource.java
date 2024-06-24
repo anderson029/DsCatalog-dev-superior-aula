fbda8cf819fd80fbfd42f0acafcb9a8e9634aadc
anderson029 <andersonfoliveira@yahoo.com>
2024-06-24 09:06:25 -0300

    01-22 pagination

--- backend/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -2,18 +2,17 @@ package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.DTOs.CategoryRequestDto;
 import com.devsuperior.dscatalog.DTOs.CategoryResponseDTO;
-import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -23,8 +22,14 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryResponseDTO>> findall(){
-        List<CategoryResponseDTO> list = categoryService.findAll();
+    public ResponseEntity<Page<CategoryResponseDTO>> findall(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+            ){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryResponseDTO> list = categoryService.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
