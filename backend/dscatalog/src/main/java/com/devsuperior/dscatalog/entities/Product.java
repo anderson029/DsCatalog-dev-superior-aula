41803a8d6f2debe27b46dcd3674683b27115ada3
anderson029 <andersonfoliveira@yahoo.com>
2024-06-24 10:55:17 -0300

    01-23/24 product_entity, SQL script

---
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -0,0 +1,112 @@
+package com.devsuperior.dscatalog.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @Column(columnDefinition = "TEXT") //mapear como text para aceitar texto longos, por padrão é o varchar
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE") //o banco de dados armazenará o timestamp exatamente como é fornecido, sem aplicar conversões de fuso horário
+    private Instant date;
+
+    @ManyToMany
+    @JoinTable( name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"), // pega o id da própria classe(product_id)
+            inverseJoinColumns = @JoinColumn(name = "category_id")) // referencia a classe que esta 'setado' ex: (Cateogry)
+    Set<Category> categories = new HashSet<>(); //    Set é uma interface que não permite repetições. Por ser uma interface, precisamos instanciar uma classe que a implemente para utilizá-la.
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product product)) return false;
+        return Objects.equals(getId(), product.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
