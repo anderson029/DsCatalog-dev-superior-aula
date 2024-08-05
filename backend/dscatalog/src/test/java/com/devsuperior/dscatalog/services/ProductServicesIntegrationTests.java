2c3373c289f022212b0ce837f7c52d83681508ea
anderson029 <andersonfoliveira@yahoo.com>
2024-08-05 13:32:58 -0300

    02-37 Teste de integração para findAllPaged

--- backend/dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServicesIntegrationTests.java
+++ backend/dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServicesIntegrationTests.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.DTOs.ProductResponseDTO;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -7,6 +8,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
 public class ProductServicesIntegrationTests {
@@ -39,6 +44,36 @@ public class ProductServicesIntegrationTests {
     Assertions.assertThrows(ResourceNotFoundException.class, () ->{
       productService.deleteProduct(idNotExistis);
     });
+  }
+
+  @Test
+  void findAllPagedShouldReturnPageWhenPage0Size10() throws Exception {
+    Pageable pageable = PageRequest.of(0,10);
+    Page<ProductResponseDTO> pageProducts = productService.findAllPaged(pageable);
+
+    Assertions.assertNotNull(pageProducts);
+    Assertions.assertFalse(pageProducts.isEmpty());
+    Assertions.assertEquals(0, pageProducts.getNumber());
+    Assertions.assertEquals(10, pageProducts.getSize());
+    Assertions.assertEquals(25, pageProducts.getTotalElements());
+
+  }
+
+  @Test
+  void findAllPagedShouldReturnPageEmptyWhenPageNotExists() throws Exception {
+    Pageable pageable = PageRequest.of(4,10);
+    Page<ProductResponseDTO> pageProducts = productService.findAllPaged(pageable);
+
+    Assertions.assertTrue(pageProducts.isEmpty());
+  }
+
+  @Test
+  void findAllPagedShouldReturnSortedPageWhenSortByName() throws Exception {
+    Pageable pageable = PageRequest.of(0,10, Sort.by("name"));
+    Page<ProductResponseDTO> pageProducts = productService.findAllPaged(pageable);
+
+    Assertions.assertFalse(pageProducts.isEmpty());
+    Assertions.assertEquals("Macbook Pro", pageProducts.getContent().get(0).getName());
   }
 }
 
