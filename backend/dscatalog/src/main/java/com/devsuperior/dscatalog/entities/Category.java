3aa136da367e289e584daf4829224bd89a4fff70
anderson029 <andersonfoliveira@yahoo.com>
2024-08-12 11:04:40 -0300

    03-04 Implementando entidades User e Role

--- backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -5,7 +5,9 @@ import jakarta.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 
 //TODO Criar relacionamento com banco de dados
@@ -26,6 +28,9 @@ public class Category implements Serializable {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updateAt;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -66,6 +71,10 @@ public class Category implements Serializable {
 
     public Instant getUpdateAt() {
         return updateAt;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
